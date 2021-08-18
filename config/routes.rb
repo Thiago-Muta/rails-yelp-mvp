ab61f5eb50301cbbd8860eab27c8c53d15ccbde6
Thiago Muta <thimuta@hotmail.com>
2021-08-18 19:38:35 -0300

    update de tentativa delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # root to: 'restaurants#index'
-  resources :restaurants, only: [:index,:show, :new, :create, :destroy] do
+  resources :restaurants, only: [:index,:show, :new, :create] do
 # , :destroy
     resources :reviews, only: [:new, :create]
   end
-
 end
